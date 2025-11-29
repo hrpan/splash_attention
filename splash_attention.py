5fac3fe97fef1141cf74f3ce59ece7d7364aec63
Hsiao-Ru Pan <hpan@tuebingen.mpg.de>
2025-11-29 15:43:24 +0100

    formatting, more test, prepare for bwd

--- splash_attention.py
+++ splash_attention.py
@@ -10,7 +10,14 @@ import sparse_attention
 
 
 @helion.kernel(autotune_effort="none", ignore_warnings=[helion.exc.TensorOperationInWrapper])
-def _sparse_attn_fwd(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, causal: hl.constexpr = False, sample: hl.constexpr = False, return_map: hl.constexpr = False):
+def _sparse_attn_fwd(
+    q: torch.Tensor,
+    k: torch.Tensor,
+    v: torch.Tensor,
+    causal: hl.constexpr = False,
+    sample: hl.constexpr = False,
+    return_map: hl.constexpr = False
+):
 
     B, nh, T, hs = q.shape
 
@@ -29,7 +36,9 @@ def _sparse_attn_fwd(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, causal: 
     max_logits = torch.full((B * nh, T), float('-inf'), device=q.device, dtype=torch.float32)
     lse = torch.full((B * nh, T), 0, device=q.device, dtype=torch.float32)
 
-    # count number of non-masked elements for averaging sparsity
+    rands = torch.empty((B * nh, T, T), device=q.device, dtype=torch.float32)
+
+    # count number of non-masked elements for averaging sparsity (trivial for now, but can be useful for future)
     count = torch.zeros((B * nh, ), device=q.device, dtype=torch.float32)
 
     p_mask_avg = torch.zeros((B * nh, ), device=q.device, dtype=torch.float32)
@@ -45,10 +54,8 @@ def _sparse_attn_fwd(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, causal: 
 
                 if causal:
                     causal_mask = tile_q.index[:, None] >= tile_k.index[None, :]
-                    count[tile_b] += causal_mask.sum(dim=-1).sum(dim=-1)
                     logits = torch.where(causal_mask, logits, float('-inf'))
-                else:
-                    count[tile_b] += tile_k.count * tile_q.count
+                count[tile_b] += torch.where(logits == float('-inf'), 0, 1).sum(dim=-1).sum(dim=-1)
                 p_mask_avg[tile_b] += logits.sigmoid().sum(dim=-1).sum(dim=-1)
 
                 new_max_logits = torch.maximum(_max_logits, logits.amax(dim=-1))
@@ -59,13 +66,17 @@ def _sparse_attn_fwd(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, causal: 
 
                 if sample:
                     rand = torch.logit(torch.rand_like(logits))
-                    mask = torch.where(logits + rand > 0, 1., 0.)
+                    rands[tile_b, tile_q, tile_k] = rand
+                    sparse_mask = logits + rand > 0
                 else:
-                    mask = torch.where(logits > 0, 1., 0.)
+                    sparse_mask = logits > 0
 
                 if return_map:
-                    adj[tile_b, tile_q, tile_k] = mask
-                weights = mask * exp_weights
+                    if causal:
+                        adj[tile_b, tile_q, tile_k] = (sparse_mask & causal_mask).float()
+                    else:
+                        adj[tile_b, tile_q, tile_k] = sparse_mask.float()
+                weights = torch.where(sparse_mask, exp_weights, 0)
                 curr_out = torch.matmul(weights, v[tile_b, tile_q, :])
                 out_old = _lse[:, :, None] * out[tile_b, tile_q, :] * ratio[:, :, None]
                 out[tile_b, tile_q, :] = (out_old + curr_out) / new_lse[:, :, None]
@@ -78,8 +89,10 @@ def _sparse_attn_fwd(q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, causal: 
     return out.view(B, nh, T, hs), p_mask_avg.view(B, nh), adj, max_logits, lse
 
 
-@helion.kernel(autotune_effort="none")
-def _sparse_attn_mask_bwd(q: torch.Tensor, k: torch.Tensor, causal: hl.constexpr = False):
+@helion.kernel(autotune_effort="none", ignore_warnings=[helion.exc.TensorOperationInWrapper])
+def _sparse_attn_mask_bwd(grad_mask: torch.Tensor, q: torch.Tensor, k: torch.Tensor, causal: hl.constexpr = False):
+
+    # reference impl. for mask bwd
 
     B, nh, T, hs = q.shape
 
@@ -87,11 +100,11 @@ def _sparse_attn_mask_bwd(q: torch.Tensor, k: torch.Tensor, causal: hl.constexpr
     k = k.view(-1, T, hs)
 
     scale = 1 / math.sqrt(q.size(-1))
-    grad_q = torch.empty_like(q)
-    grad_k = torch.empty_like(k)
+    grad_q = torch.zeros_like(q)
+    grad_k = torch.zeros_like(k)
 
     if causal:
-        count = float((1 + T) * T // 2)
+        count = float(T * (T + 1) // 2)
     else:
         count = float(T * T)
 
@@ -99,53 +112,110 @@ def _sparse_attn_mask_bwd(q: torch.Tensor, k: torch.Tensor, causal: hl.constexpr
         for tile_q in hl.tile(T):
             qs = q[tile_b, tile_q, :]
             for tile_k in hl.tile(T):
+
                 ks = k[tile_b, tile_k, :]
                 logits = (qs @ ks.transpose(-1, -2) * scale)
 
                 prob = logits.sigmoid()
-                grad_sigmoid = prob * (1 - prob) * scale
+                grad_sigmoid = grad_mask[tile_b, None, None] * prob * (1 - prob) * scale / count
+
+                if causal:
+                    causal_mask = tile_q.index[:, None] >= tile_k.index[None, :]
+                    grad_sigmoid = torch.where(causal_mask, grad_sigmoid, 0)
 
                 grad_q[tile_b, tile_q, :] = torch.baddbmm(grad_q[tile_b, tile_q, :], grad_sigmoid, ks)
                 grad_k[tile_b, tile_k, :] = torch.baddbmm(grad_k[tile_b, tile_q, :], grad_sigmoid.transpose(-1, -2), qs)
 
-    grad_q = grad_q.view(B, nh, T, hs) / count
-    grad_k = grad_k.view(B, nh, T, hs) / count
+    grad_q = grad_q.view(B, nh, T, hs)
+    grad_k = grad_k.view(B, nh, T, hs)
     return grad_q, grad_k
 
 
 @helion.kernel(autotune_effort="none")
-def _sparse_attn_bwd(q: torch.Tensor, k: torch.Tensor, causal: hl.constexpr = False):
+def _sparse_attn_bwd(
+    grad_out: torch.Tensor,
+    grad_mask: torch.Tensor,
+    q: torch.Tensor,
+    k: torch.Tensor,
+    v: torch.Tensor,
+    out: torch.Tensor,
+    max_logits: torch.Tensor,
+    lse: torch.Tensor,
+    causal: hl.constexpr = False,
+    sample: hl.constexpr = False,
+):
 
     B, nh, T, hs = q.shape
 
+    out = out.view(-1, T, hs)
+
     q = q.view(-1, T, hs)
     k = k.view(-1, T, hs)
+    v = v.view(-1, T, hs)
+
+    grad_out = grad_out.view(-1, T, hs)
+    grad_mask = grad_mask.view(-1)
 
     scale = 1 / math.sqrt(q.size(-1))
-    grad_q = torch.empty_like(q)
-    grad_k = torch.empty_like(k)
+    grad_q = torch.zeros_like(q)
+    grad_k = torch.zeros_like(k)
+    grad_v = torch.zeros_like(v)
 
     if causal:
-        count = float((1 + T) * T // 2)
+        count = float(T * (T + 1) // 2)
     else:
         count = float(T * T)
 
     for tile_b in hl.tile(B * nh):
         for tile_q in hl.tile(T):
+            _max_logits = max_logits[tile_b, tile_q]
+            _lse = lse[tile_b, tile_q]
             qs = q[tile_b, tile_q, :]
+
             for tile_k in hl.tile(T):
                 ks = k[tile_b, tile_k, :]
                 logits = (qs @ ks.transpose(-1, -2) * scale)
 
-                prob = logits.sigmoid()
-                grad_sigmoid = prob * (1 - prob) * scale
+                if sample:
+                    rand = torch.logit(torch.rand_like(logits))
+                    sparse_mask = logits + rand > 0
+                    gate = (logits + rand).sigmoid()
+                    gate_prob = logits.sigmoid()
+                else:
+                    sparse_mask = logits > 0
+                    gate = logits.sigmoid()
+                    gate_prob = gate
+                #logits = torch.where(sparse_mask, logits, float('-inf'))
 
+                if causal:
+                    causal_mask = tile_q.index[:, None] >= tile_k.index[None, :]
+                    logits = torch.where(causal_mask, logits, float('-inf'))
+
+                attn_weights = torch.exp(logits - _max_logits[:, :, None]) / _lse[:, :, None]
+                final_weights = torch.where(sparse_mask, attn_weights, 0)
+
+                grad_v[tile_b, tile_k, :] = torch.baddbmm(
+                    grad_v[tile_b, tile_k, :],
+                    final_weights.transpose(-1, -2),
+                    grad_out[tile_b, tile_k, :]
+                )
+
+                gvt = torch.matmul(grad_out[tile_b, tile_q, :], v[tile_b, tile_k, :].transpose(-1, -2))
+                go = (grad_out[tile_b, tile_q, :] * out[tile_b, tile_q, :]).sum(dim=-1)[:, :, None]
+
+                grad_q[tile_b, tile_q, :] = torch.baddbmm(
+                    grad_q[tile_b, tile_q, :],
+                    attn_weights * (gvt * gate * (2 - gate) - go),
+                    scale * k[tile_b, tile_k, :]
+                )
+
+                # sparsity bwd
+                grad_sigmoid = grad_mask[tile_b, None, None] * gate_prob * (1 - gate_prob) * scale / count
                 grad_q[tile_b, tile_q, :] = torch.baddbmm(grad_q[tile_b, tile_q, :], grad_sigmoid, ks)
-                grad_k[tile_b, tile_k, :] = torch.baddbmm(grad_k[tile_b, tile_q, :], grad_sigmoid.transpose(-1, -2), qs)
+                grad_k[tile_b, tile_k, :] = torch.baddbmm(grad_k[tile_b, tile_k, :], grad_sigmoid.transpose(-1, -2), qs)
 
-    grad_q = grad_q.view(B, nh, T, hs) / count
-    grad_k = grad_k.view(B, nh, T, hs) / count
-    return grad_q, grad_k
+    #grad_k = grad_k.view(B, nh, T, hs) / count
+    return grad_q.view(B, nh, T, hs), None, grad_v.view(B, nh, T, hs)
 
 
 class SparseAttention(torch.autograd.Function):
@@ -153,13 +223,14 @@ class SparseAttention(torch.autograd.Function):
     def forward(ctx, q, k, v, causal: bool = False, sample: bool = False, return_map: bool = False):
 
         assert q.shape == k.shape and k.shape == v.shape
+        ctx.causal = causal
+        ctx.sample = sample
+        ctx.cuda_rng = torch.cuda.get_rng_state(device=q.device)
 
         out, p_mask, adj, max_logits, lse = _sparse_attn_fwd(q, k, v, causal, sample, return_map)
 
         ctx.save_for_backward(q, k, v, out, p_mask, max_logits, lse)
-        ctx.causal = causal
-        ctx.sample = sample
-        #if sample:
+                #if sample:
         #    ctx.mark_non_differentiable(adj)
         #else:
         #    ctx.mark_non_differentiable(out, adj)
@@ -174,8 +245,11 @@ class SparseAttention(torch.autograd.Function):
         sample = ctx.sample
 
         grad_q = grad_k = grad_v = None
-        grad_q, grad_k = _sparse_attn_mask_bwd(q, k, causal)
-        grad_v = torch.zeros_like(v)
+        with torch.random.fork_rng(devices=[q.device]):
+            torch.cuda.set_rng_state(ctx.cuda_rng, q.device)
+            grad_q, _, grad_v = _sparse_attn_bwd(grad_out, grad_p_mask, q, k, v, out, max_logits, lse, causal, sample)
+
+        #grad_q, grad_k = _sparse_attn_mask_bwd(grad_p_mask, q, k, causal)
 
         return grad_q, grad_k, grad_v, None, None, None
 
@@ -184,14 +258,55 @@ sparse_attention_ = SparseAttention.apply
 
 if __name__ == '__main__':
 
+    eps = 1e-2  # from helion puzzle
+
     q, k, v = torch.randn(3, 1, 2, 10, 10, device='cuda', dtype=torch.float32).unbind(0)
 
     # causal attention test
-    out, p_mask, adj = sparse_attention_(q, k, v, True, False, False)
-    gold_out, gold_p_mask, gold_adj = sparse_attention._sparse_attention_torch(q, k, v, True, False, False)
-    print('causal: ', torch.allclose(out, gold_out, atol=1e-2, rtol=1e-2), (gold_out - out).abs().max())
+    with torch.no_grad():
+        out, p_mask, adj = sparse_attention_(q, k, v, True, False, False)
+        gold_out, gold_p_mask, gold_adj = sparse_attention._sparse_attention_torch(q, k, v, True, False, False)
+    print('causal forward test: ')
+    assert torch.allclose(out, gold_out, atol=eps, rtol=eps)
+    print('out passed with abs diff:', (gold_out - out).abs().max())
+    assert torch.allclose(p_mask, gold_p_mask, atol=eps, rtol=eps)
+    print('mask passed with abs diff:', (gold_p_mask - p_mask).abs().max())
 
     # noncausal attention test
-    h, p_mask, adj = sparse_attention_(q, q, q, False, False, False)
-    gold, gold_p_mask, gold_adj = sparse_attention._sparse_attention_torch(q, k, v, False, False, False)
-    print('noncausal: ', torch.allclose(out, gold_out, atol=1e-2, rtol=1e-2), (gold_out - out).abs().max())
+    with torch.no_grad():
+        out, p_mask, adj = sparse_attention_(q, k, v, False, False, False)
+        gold_out, gold_p_mask, gold_adj = sparse_attention._sparse_attention_torch(q, k, v, False, False, False)
+    print('noncausal forward test: ')
+    assert torch.allclose(out, gold_out, atol=eps, rtol=eps)
+    print('out passed with abs diff:', (gold_out - out).abs().max())
+    assert torch.allclose(p_mask, gold_p_mask, atol=eps, rtol=eps)
+    print('mask passed with abs diff:', (gold_p_mask - p_mask).abs().max())
+
+
+    # mask backward test
+    q.requires_grad = True
+    k.requires_grad = True
+    v.requires_grad = True
+    q2 = q.detach().clone()
+    k2 = k.detach().clone()
+    v2 = v.detach().clone()
+    q2.requires_grad = True
+    k2.requires_grad = True
+    v2.requires_grad = True
+    out, p_mask, adj = sparse_attention_(q, k, v, False, False, False)
+    gold_out, gold_p_mask, gold_adj = sparse_attention._sparse_attention_torch(q2, k2, v2, False, False, False)
+    #(out.sum() + p_mask.sum()).backward()
+    #(gold_out.sum() + gold_p_mask.sum()).backward()
+    out.sum().backward()
+    gold_out.sum().backward()
+    print('noncausal backward test: ')
+    grad_diff = (q.grad - q2.grad).abs().max()
+    assert torch.allclose(q.grad, q2.grad, atol=eps, rtol=eps), f'q.grad failed abs max: {grad_diff:.4f}'
+    print('q.grad passed with abs diff:', grad_diff)
+    #grad_diff = (k.grad - k2.grad).abs().max()
+    #assert torch.allclose(k.grad, k2.grad, atol=eps, rtol=eps), f'k.grad failed abs max: {grad_diff:.4f}'
+    #print('k.grad passed with abs diff:', grad_diff)
+    #print(v.grad, v2.grad)
+    grad_diff = (v.grad - v2.grad).abs().max()
+    assert torch.allclose(v.grad, v2.grad, atol=eps, rtol=eps), f'v.grad failed abs max: {grad_diff:.4f}'
+    print('v.grad passed with abs diff:', grad_diff)

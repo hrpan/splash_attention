1be883210daa4ca0639216119e7a42b50f4c13ad
Hsiao-Ru Pan <hpan@tuebingen.mpg.de>
2026-01-27 15:25:07 +0100

    add init

---
+++ src/splash_attention/__init__.py
@@ -0,0 +1,4 @@
+from splash_attention.sparse_attention import sparse_attention_naive
+from splash_attention.splash_attention import splash_attention
+
+__all__ = ['sparse_attention_naive', 'splash_attention']
